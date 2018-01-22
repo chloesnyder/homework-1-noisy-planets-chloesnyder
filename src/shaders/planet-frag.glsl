f3a94806611fef1f84cadf75ad5b256a73a5c931
Chloe Brown Snyder <csny@seas.upenn.edu>
2018-01-22 16:20:07 -0500

    my code works i don’t know why

--- src/shaders/planet-frag.glsl
+++ src/shaders/planet-frag.glsl
@@ -23,31 +23,11 @@ in vec4 fs_Pos;
 out vec4 out_Col; // This is the final output color that you will see on your
                   // screen for the pixel that is currently being processed.
 
-/*void main()
-{
-    // Material base color (before shading)
-        vec4 diffuseColor = u_Color;
-
-        // Calculate the diffuse term for Lambert shading
-        float diffuseTerm = dot(normalize(fs_Nor), normalize(fs_LightVec));
-        // Avoid negative lighting values
-        // diffuseTerm = clamp(diffuseTerm, 0, 1);
-
-        float ambientTerm = 0.2;
-
-        float lightIntensity = diffuseTerm + ambientTerm;   //Add a small float value to the color multiplier
-                                                            //to simulate ambient lighting. This ensures that faces that are not
-                                                            //lit by our point light are not completely black.
-
-        // Compute final shaded color
-        out_Col = vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a);
-}*/
 
 const vec4 sphereCenter = vec4(0.f,0.f,0.f,1.f);
 
 vec2 convertToUV(vec4 sphereSurfacePt, vec4 sphereCenterPt)
 {
-
     vec4 d = normalize(sphereSurfacePt - sphereCenterPt);
     float phi = atan(d.z, d.x);
     if(phi < 0.f) phi += PI * 2.f;
@@ -56,36 +36,31 @@ vec2 convertToUV(vec4 sphereSurfacePt, vec4 sphereCenterPt)
     return vec2(1.f - phi / PI, 1.f - theta / PI);
 }
 
-vec3 squareToSphere(vec2 v)
-{   
-    float theta = v.x;
-    float phi = v.y;
-    float x = sin(theta) * cos(phi);
-    float y = sin(theta) * sin(phi);
-    float z = 1.f - (2.f * cos(theta));
-    float e1 = cos(theta);
-    float e2 = phi / (2.f * PI);
-    float xnew = cos(2.f*PI*e2) * sqrt(1.f - z * z);
-    float ynew = sin(2.f*PI*e2) * sqrt(1.f - z * z);
-    float znew = 1.f - 2.f*e1;
-    return vec3(xnew,ynew,znew);
+vec3 random3D (vec3 st) {
+    float x = fract(sin(dot(st.xyz,
+                         vec3(12.9898,78.233,78.233)))*
+        52758.5453123);
+    float y = fract(sin(dot(st.xyz,
+                         vec3(134578989.8,7131.233,78.233)))*
+        454.53123);
+    float z = fract(sin(dot(st.xyz,
+                         vec3(18.23498,72.25333,5438.233)))*
+        43714791.53123);
+    return(vec3(x,y,z));
 }
 
-/*float greatCircleDistance (vec3 a, vec3 b)
-{
-   return (cross(a, b) / dot(a, b));
-}*/
- 
+vec3 random3DTest(vec3 st) {
+    return vec3(fract(sin(dot(st, vec3(12.9898, 78.233, 56.176)) * 43758.5453)));
+}
 
 void main() {
     vec2 st = convertToUV(fs_Pos, sphereCenter);
-   // st.x /= .5f;
-    //st.x *= u_resolution.x/u_resolution.y;
+ 
 
     vec3 color = vec3(.0);
 
-    // Cell positions
-    vec3 point[7];
+  /*  // Cell positions
+    vec3 point[30];
     point[0] = .5 * vec3(1.0, 1.0, 1.0);
     point[1] = .5 * vec3(-1.0, 1.0, 1.0);
     point[2] = .5 * vec3(1.0, -1.0, 1.0);
@@ -94,12 +69,19 @@ void main() {
     point[5] = .5 * vec3(1.0, -1.0, -1.0);
     point[6] = .5 * vec3(-1.0, -1.0, -1.0);
 
+    //now add some random points
+
+   for(int i = 0; i < 30; i++)
+    {
+        point[i] = (random(vec3(float(i) / 30.f, 30.f - float(i) / 30.f, float(i*i)/30.f)));
+    }
+
 
     float m_dist = 1.;  // minimun distance
 
     // 3D 
     // Iterate through the points positions
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < 30; i++) {
        // vec3 spherePt = squareToSphere(point[i]);
         float dist = distance(vec3(fs_Pos),point[i]);
 
@@ -108,10 +90,30 @@ void main() {
     }
 
     // Draw the min distance (distance field)
-    color += m_dist;
+    color += vec3(1.) - vec3(m_dist);
 
     // Show isolines
     // color -= step(.7,abs(sin(50.0*m_dist)))*.3;
 
+    out_Col = vec4(color,1.0);*/
+
+
+    float scalar = 5.0;
+    vec3 gridSpacePoint = fs_Pos.xyz * scalar; // Scalar can be 1 for now for testing
+    float minDist = 10.0;
+    for(int i = -1; i <= 1; ++i)
+    {
+        for(int j = -1; j <= 1; ++j)
+        {
+            for(int k = -1; k <= 1; ++k)
+            {
+                vec3 gridCellCorner = floor(gridSpacePoint) + vec3(float(i), float(j), float(k));
+                vec3 worleyPoint = random3D(gridCellCorner);
+                float dist = distance(worleyPoint + gridCellCorner, gridSpacePoint);
+                minDist = min(minDist, dist);
+            }
+        }
+    }
+    color = vec3(minDist);
     out_Col = vec4(color,1.0);
 }
